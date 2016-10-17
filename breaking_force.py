b10445f7be8c6e008f0e150b29b4c46128c816ea
amaltz1 <aubreymaltz@gmail.com>
2016-10-17 13:58:22 -0400

    Updated constantsand function for Bfield from a 1 cubic inch cubic magnet

--- breaking_force.py
+++ breaking_force.py
@@ -1,7 +1,30 @@
-#Objective: return the new distance between the rail such that the braking de-acceleration
-#does not exceed 2.4G. Note that before the inflection point, friction braking will be used
-#The braking force is based on the speed of the pod and the surface current density of
-#on the rail, which is based on the distance between the magnet and the rail
+'''
+
+Objective: return the new distance between the rail such that the braking de-acceleration
+does not exceed 2.4G. Note that before the inflection point, friction braking will be used
+The braking force is based on the speed of the pod and the surface current density of
+on the rail, which is based on the distance between the magnet and the rail
+
+maximum separation distance 12-15mm
+minimum separation distance 6mm
+'''
+
+from math import *
+
+podMass = 250 #mass of pod is 250 kg assuming there are no passengers, there is a scale that will measure this
+mu = 1.25e-6             #mag perm of aluminum
+sigma = 36.9e6     #electrical conductivity of aluminum
+T = 0.0079502          #thickness of rails 
+vp = 2/(mu*sigma*T) #"drag peak" velocity (before drag peak, friction braking takes over, after )
+Br = 1.47 #remanence of N52 grade neodymium magnets (in teslas). This is effectively the point magnetic field of the magnet
+L = 0.0254 #dimensions of the block magnets (1x1x1 inch cube or 0.0254 m^3 cube)
+
+
+def Bfield_cube(z):
+	#returns the b-field at a point z meters away from the 1 cubic inch magnets
+	B = (Br/pi)*(atan(L**2/(2*z*sqrt(4*z**2+2*L**2)))-atan(L**2/(2*(L+z)*sqrt(4*(L+z)**2+2*L**2))))
+	return(B)
+
 
 def new_distance(v,z):
 	#something
