998dfb2e968e9cddbb8ee917b54656fb76eebc67
amaltz1 <aubreymaltz@gmail.com>
2016-10-17 02:16:00 -0400

    added a description to the function handle

---
+++ breaking_force.py
@@ -0,0 +1,7 @@
+#Objective: return the new distance between the rail such that the braking de-acceleration
+#does not exceed 2.4G. Note that before the inflection point, friction braking will be used
+#The braking force is based on the speed of the pod and the surface current density of
+#on the rail, which is based on the distance between the magnet and the rail
+
+def new_distance(v,z):
+	return()
