a927ebf29ab82886d14df772b25856de09c5682b
amaltz1 <aubreymaltz@gmail.com>
2016-10-17 14:30:07 -0400

    made changes to braking force based on thomsonrd report

--- breaking_force.py
+++ breaking_force.py
@@ -14,7 +14,7 @@ from math import *
 podMass = 250 #mass of pod is 250 kg assuming there are no passengers, there is a scale that will measure this
 mu = 1.25e-6             #mag perm of aluminum
 sigma = 36.9e6     #electrical conductivity of aluminum
-T = 0.0079502          #thickness of rails 
+t = 0.0079502          #thickness of rails 
 vp = 2/(mu*sigma*T) #"drag peak" velocity (before drag peak, friction braking takes over, after )
 Br = 1.47 #remanence of N52 grade neodymium magnets (in teslas). This is effectively the point magnetic field of the magnet
 L = 0.0254 #dimensions of the block magnets (1x1x1 inch cube or 0.0254 m^3 cube)
@@ -25,7 +25,11 @@ def Bfield_cube(z):
 	B = (Br/pi)*(atan(L**2/(2*z*sqrt(4*z**2+2*L**2)))-atan(L**2/(2*(L+z)*sqrt(4*(L+z)**2+2*L**2))))
 	return(B)
 
+def Braking_force(v,z):
+	B = Bfield_cube(z) #B varies with separation distance
+	if (v > 2/(sigma*mu*t)):
+        return (1/v)*B**2*a*t*sigma*(1.75/2)
 
 def new_distance(v,z):
-	#something
+	maxforce = podMass * 9.812 * 2.4
 	return()
