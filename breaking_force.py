20f906256826bb0dc3d6743fd5a61224124fb69e
amaltz1 <aubreymaltz@gmail.com>
2016-11-03 15:25:39 -0400

    added a matrix relating B field to separation distance

--- breaking_force.py
+++ breaking_force.py
@@ -18,47 +18,56 @@ http://www.convertit.com/go/educationplanet/reference/ams55.asp?Res=200&Page=80
 '''
 
 from math import *
-
+import numpy as np
 podMass = 250 #mass of pod is 250 kg assuming there are no passengers, there is a scale that will measure this
 mu = 1.25e-6             #mag perm of aluminum
 sigma = 36.9e6     #electrical conductivity of aluminum
 t = 0.0079502          #thickness of rails 
-vp = 2/(mu*sigma*T) #"drag peak" velocity (before drag peak, friction braking takes over, after )
+vp = 2/(mu*sigma*t) #"drag peak" velocity (before drag peak, friction braking takes over, after )
 Br = 1.47 #remanence of N52 grade neodymium magnets (in teslas). This is effectively the point magnetic field of the magnet
 L = 0.0254 #dimensions of the block magnets (1x1x1 inch cube or 0.0254 m^3 cube)
 a = 0.0541 #area exposed in the air gap of braking system <<tentative>>
-
-
+s = sigma
+datapoints = 1000
 maxforce = podMass * 9.812 * 2.4
-
+z_min = 6e-3
+z_max = 15e-3
+z_vals = np.linspace(z_min,z_max,datapoints)
+B_vals = [0]*(datapoints)
 
 def Bfield_cube(z):
 	#returns the b-field at a point z meters away from the 1 cubic inch magnets
 	B = (Br/pi)*(atan(L**2/(2*z*sqrt(4*z**2+2*L**2)))-atan(L**2/(2*(L+z)*sqrt(4*(L+z)**2+2*L**2))))
 	return(B)
 
+for i in range(len(z_vals)):
+    B_vals[i] = float(Bfield_cube(z_vals[i]))
+ 
+ 
 def Braking_force(v,z):
-	b = Bfield_cube(z) #B varies with separation distance
-	if (v > 2/(sigma*mu*t)):
-        return (1/v)*b**2*a*t*sigma*(1.75/2)
-	 else:
+    b = Bfield_cube(z) #B varies with separation distance
+    if (v > 2/(sigma*mu*t)):
+         return (1/v)*b**2*a*t*sigma*(1.75/2)
+    else:
         return (v)*b**2*a*t*s*(1.75/2)
 
 
-def Separation_Distance(F,v):
+def Separation_Distance(v):
 	#returns the separation distance between the magnets in the rail such that at speed v, constraint force F is maintained 
 	#assuming that the velocity does not constantly change in the moment that the magnet adjustment period by the actuator
-
-	if v < 2/(sigma*mu*t):
-        return (v)*b**2*a*t*s*(1.75/2)
+    F = maxforce
+    if v < 2/(sigma*mu*t):
+        #B_req = sqrt(2 * F  / ( v *a * t * sigma * 1.75))
+        return() #If Velocity is below peak, no braking force
     else:
-    	B_req = sqrt(2 * F * v / ( a * t * sigma * 1.75))
+    	    B_req = sqrt(2 * F * v / ( a * t * sigma * 1.75))
+    z_index = min(range(len(B_vals)), key=lambda i: abs(B_vals[i]-B_req)) #find z index with assosciated B field value closest to B_req
     	#z_req = newton_krylov (use newton_krylov method to solve nonlinear equation for z)
+    return(z_vals[z_index])
 
+    
 
-#hello there its geoffrey
-#hello there its aubrey
-
+     
 
 
 
